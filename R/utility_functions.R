c5383ad737c5e511a1468e89b8dce7b47d58a700
atfrank <afrankz@umich.edu>
2016-08-02 11:00:38 -0400

    library loader
    add a new utility function that loads a package if already installed or
    install it and then loads it, if it is not already installed

--- R/utility_functions.R
+++ R/utility_functions.R
@@ -1,5 +1,19 @@
+load_library <- function(package_name, repo='http://cran.mtu.edu/'){
+	#' Package Loader
+	#'
+	#' This function load a package if installed, and install then loads if not
+	#' @param package_name name (string) of package	
+  #' @param repo repository (string) to use when installing the package	
+	#' @export
+	#' @examples
+	#' load_library(package_name='plyr')
+
+   if(package_name %in% rownames(installed.packages()) == FALSE) {install.packages(package_name,dep=TRUE,repos=repo)}
+   library(package_name)
+}
+
 update_package <- function(package_path='/Users/atfrank/GitSoftware/nmR', package_name='nmR'){
-	#' Packahe Documentation Update Function
+	#' Package Documentation Update Function
 	#'
 	#' This function updates the package specified by package_path and package_name
 	#' @param package_path path (string) to package
