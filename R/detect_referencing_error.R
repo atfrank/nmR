e5ec7e599117f97235b0a99e0ae6083efb3ff3da
Aaron Frank <afrankz@umich.edu>
2016-12-08 11:55:33 -0500

    updated automatic referencing functions...

--- R/detect_referencing_error.R
+++ R/detect_referencing_error.R
@@ -8,8 +8,8 @@ detect_referencing_error <- function(observed_chemical_shifts, computed_chemical
   #' @param verbose if TRUE, print progress log from MCMCpack. Default is FALSE
   #' @export
   #' @examples
-  #' add_cs_weights(cs, weights, residue_and_nucleus_based=TRUE)
-  #' add_cs_weights(cs, weights, residue_and_nucleus_based=FALSE)
+  #' detect_referencing_error(observed_chemical_shifts, computed_chemical_shifts, residue_and_nucleus_based=TRUE)
+  #' detect_referencing_error(observed_chemical_shifts, computed_chemical_shifts, residue_and_nucleus_based=FALSE)
   require(MCMCpack)
   cs <- merge(observed_chemical_shifts, computed_chemical_shifts)
   cs$secondary <- cs$expCS - cs$predCS
@@ -19,4 +19,34 @@ detect_referencing_error <- function(observed_chemical_shifts, computed_chemical
     f <- MCMCregress(secondary~nucleus+resname+0, data = cs, burnin = 1000, mcmc = 10000, thin = 1, verbose = verbose)
   }
   return(summary(f))
+}
+
+
+
+correct_referencing_error <- function(observed_chemical_shifts, computed_chemical_shifts, verbose = FALSE){
+  #' Correct Referencing Errors
+  #'
+  #' This function allows user to detect potential referencing errors by comparing observed and computed chemical shifts
+  #' @param observed_chemical_shifts observed chemical shift dataframe. Should contain field: resname, resid, nucleus, expCS
+  #' @param computed_chemical_shifts observed chemical shift dataframe. Should contain field: resname, resid, nucleus, predCS
+  #' @param verbose if TRUE, print progress log from MCMCpack. Default is FALSE
+  #' @export
+  #' @examples
+  #' correct_referencing_error(observed_chemical_shifts, computed_chemical_shifts)
+
+  err <- detect_referencing_error(observed_chemical_shifts, computed_chemical_shifts, FALSE, verbose)
+  err <- as.data.frame(err$statistics)
+  err$ratio <- abs(err$Mean/err$SD)
+  err <- err[((err$ratio>2) & (abs(err$Mean) > 1)),]
+  err <- err[!(rownames(err) %in% "sigma2"),]
+  err$nucleus <- substr(rownames(err), 8, 12)
+  err$Mean <- round(err$Mean, 3)
+  err <- err[,(colnames(err) %in% c("nucleus", "Mean"))]
+  observed_chemical_shifts <- merge(observed_chemical_shifts, err, all = TRUE)
+  observed_chemical_shifts$Mean[is.na(observed_chemical_shifts$Mean)] <- 0.0
+  observed_chemical_shifts$Mean <- abs(observed_chemical_shifts$Mean)
+  observed_chemical_shifts$cs_corrected <- observed_chemical_shifts$expCS + abs(observed_chemical_shifts$Mean)
+  expnames <- c("resname","resid","nucleus","cs_corrected","error")
+  observed_chemical_shifts <- observed_chemical_shifts[,expnames]
+  return(list(shifts=observed_chemical_shifts, errors=err))
 }
