ff19c167269727eba689c8f7531a0cc74f44c1e0
Aaron Frank <afrankz@umich.edu>
2018-07-01 02:58:52 -0400

    tweak to make L1 regularization work for real values between 0 and 1

--- R/selection.R
+++ R/selection.R
@@ -27,7 +27,7 @@ fitness <- function(p, mask = NULL, weights = 1, alpha = 10){
   p <- p/sum(p)
   if (!is.null(mask)){p[mask] <- 0}
   ensemble_averaged <- get_ensemble_average(p, ensemble)
-  return(-mean(weights*abs(ensemble_averaged-target))-alpha*(sum(p))) # Note that this is a L1 regularized optimization
+  return(-mean(weights*abs(ensemble_averaged-target))-alpha*(sum(p>0.01))) # Note that this is a L1 regularized optimization
 }
 
 run_ga_selection <- function(target, ensemble, cycles = 100, population_size = 100, seed = 12345, binary = TRUE, weights = 1, monitor = FALSE, alpha = 10){
